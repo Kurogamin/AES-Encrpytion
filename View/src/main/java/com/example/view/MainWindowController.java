6759c599666da768dc1a874d32e29ebbfa7dcd60
236665 Strzelecki Mateusz <236665@edu.p.lodz.pl>
2022-05-14 18:02:48 +0200

    conect model and view

--- View/src/main/java/com/example/view/MainWindowController.java
+++ View/src/main/java/com/example/view/MainWindowController.java
@@ -1,6 +1,9 @@
 package com.example.view;
 
-import Model.Class;
+import Model.AES;
+import Model.IO;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,14 +13,22 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Random;
 
 public class MainWindowController {
 
     private Stage stage;
     private Scene scene;
+
+    private AES aes;
 
     private File fileToEncrypt;
     private File fileToDecrypt;
@@ -25,6 +36,7 @@ public class MainWindowController {
 
     MainWindowController() {
         stage = new Stage();
+        aes = new AES();
         try {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindow.fxml"));
         loader.setController(this);
@@ -49,52 +61,84 @@ public class MainWindowController {
 
         selectFileToDecryptButton.setOnAction(actionEvent -> fileToDecrypt = selectFileButtonHandler(fileNameToDecryptTextField));
         selectFileToEncryptButton.setOnAction(actionEvent -> fileToEncrypt = selectFileButtonHandler(fileToEncryptNameTextField));
+
+        addTextLimiter(decryptedMessageKeyTextField, 16);
+        addTextLimiter(encryptedMessageKeyTextField, 16);
+        addTextLimiter(encryptedFileKeyTextField, 16);
+        addTextLimiter(decryptedFileKeyTextField, 16);
+
+        generateKeyEncryptFileButton.setOnAction(actionEvent -> encryptedFileKeyTextField.setText(generateKey(16)));
+        generateKeyDecryptFileButton.setOnAction(actionEvent -> decryptedFileKeyTextField.setText(encryptedFileKeyTextField.getText()));
+
+        generateKeyEncryptMesButton.setOnAction(actionEvent -> encryptedMessageKeyTextField.setText(generateKey(16)));
+        generateKeyDecryptMesButton.setOnAction(actionEvent -> decryptedMessageKeyTextField.setText(encryptedMessageKeyTextField.getText()));
+
+        copyEncryptedMessageButton.setOnAction(actionEvent -> copyToClipboard(encryptedTextArea));
+        copyDecryptedMessageButton.setOnAction(actionEvent -> copyToClipboard(decryptedTextArea));
     }
 
     private void encryptMessageButtonHandler() {
         String message = messageToEncryptTextArea.getText();
-        if (!message.isEmpty()) {
-            //TODO
-            errorEncryptMessageLabel.setText("");
-        } else {
-            errorEncryptMessageLabel.setText("Message to encrypt is empty");
-        }
+        String key = encryptedMessageKeyTextField.getText();
 
+        if(checkKey(key, 16, errorEncryptMessageLabel)) {
+            if (!message.isEmpty()) {
+                byte[] bytes = aes.cipher(message.getBytes(StandardCharsets.UTF_8), key, true);
+                String encryptedMessage = Base64.getEncoder().encodeToString(bytes);
+                encryptedTextArea.setText(encryptedMessage);
+                errorEncryptMessageLabel.setText("");
+            } else {
+                errorEncryptMessageLabel.setText("Message to encrypt is empty");
+            }
+        }
     }
 
     private void decryptMessageButtonHandler() {
         String message = messageToDecryptTextArea.getText();
         String key = decryptedMessageKeyTextField.getText();
 
-        if (!message.isEmpty() && !key.isEmpty()) {
-            //TODO
-            errorDecryptMessageLabel.setText("");
-        } else {
-            errorDecryptMessageLabel.setText("Message to decrypt or key are empty");
+
+        if (checkKey(key, 16, errorDecryptMessageLabel)) {
+            if (!message.isEmpty() && !key.isEmpty()) {
+                byte[] bytes = aes.cipher(Base64.getDecoder().decode(message.getBytes(StandardCharsets.UTF_8)), key, false);
+                String decryptedMessage = new String(bytes, StandardCharsets.UTF_8);
+                decryptedTextArea.setText(decryptedMessage);
+                errorDecryptMessageLabel.setText("");
+            } else {
+                errorDecryptMessageLabel.setText("Message to decrypt or key are empty");
+            }
         }
     }
 
     private void encryptFileButtonHandler() {
+        String key = encryptedFileKeyTextField.getText();
 
-        if (fileToEncrypt != null) {
-
-            //TODO
-            errorEncryptFileLabel.setText("");
-        } else {
-            errorEncryptFileLabel.setText("Select file to encrypt");
+        if (checkKey(key, 16, errorEncryptFileLabel)) {
+            if (fileToEncrypt != null && !key.isEmpty() && !fileNameToEncryptTextField.getText().isEmpty()) {
+                byte [] file = IO.readFileToByteArray(fileToEncrypt.getAbsolutePath());
+                byte [] encryptedFile = aes.cipher(file, key, true);
+                //byte [] decryptedFile = aes.cipher(encryptedFile, key, false);
+                IO.writeFileFromByteArray(encryptedFile, fileToEncrypt.getParent() + "\\" + fileNameToEncryptTextField.getText());
+                errorEncryptFileLabel.setText("");
+            } else {
+                errorEncryptFileLabel.setText("Select file to encrypt or name");
+            }
         }
-
     }
 
     private void decryptFileButtonHandler() {
         String key = decryptedFileKeyTextField.getText();
-        if (fileToDecrypt != null && !key.isEmpty()) {
-            //TODO
-            errorDecryptFileLabel.setText("");
-        } else {
-            errorDecryptFileLabel.setText("Select file to decrypt or key field is empty");
-        }
 
+        if (checkKey(key, 16, errorDecryptFileLabel)) {
+            if (fileToDecrypt != null && !key.isEmpty() && !fileNameToDecryptTextField.getText().isEmpty()) {
+                byte [] file = IO.readFileToByteArray(fileToDecrypt.getAbsolutePath());
+                byte [] decryptedFile = aes.cipher(file, key, false);
+                IO.writeFileFromByteArray(decryptedFile, fileToDecrypt.getParent() + "\\" + decryptedFileNameTextArea.getText());
+                errorDecryptFileLabel.setText("");
+            } else {
+                errorDecryptFileLabel.setText("Select file to decrypt or key field is empty");
+            }
+        }
     }
 
     private File selectFileButtonHandler(TextField textField) {
@@ -105,6 +149,43 @@ public class MainWindowController {
         textField.setText(file.getName());
 
         return file;
+    }
+
+    private void addTextLimiter(final TextField tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
+    }
+
+    private boolean checkKey(String key, int charactersAmount,Label error) {
+        if(key.length() != charactersAmount) {
+            error.setText("Key is too short");
+            return false;
+        }
+        return true;
+    }
+
+    private String generateKey(int length) {
+        Random random = new Random();
+        String key = "";
+
+        for (int i = 0; i < length; i++) {
+            char sign = (char)(random.nextInt(93) + '!');
+            key += sign;
+        }
+        return key;
+    }
+
+    private void copyToClipboard(TextArea text) {
+        StringSelection stringSelection = new StringSelection (text.getText());
+        Clipboard clipboard = Toolkit.getDefaultToolkit ().getSystemClipboard ();
+        clipboard.setContents (stringSelection, null);
     }
 
     public void showStage() {
@@ -143,6 +224,18 @@ public class MainWindowController {
 
     @FXML
     private Label errorDecryptMessageLabel;
+
+    @FXML
+    private Button generateKeyEncryptMesButton;
+
+    @FXML
+    private Button generateKeyDecryptMesButton;
+
+    @FXML
+    private Button copyEncryptedMessageButton;
+
+    @FXML
+    private Button copyDecryptedMessageButton;
 
     //file
 
@@ -185,8 +278,9 @@ public class MainWindowController {
     @FXML
     private Label errorDecryptFileLabel;
 
-    //@FXML
-    //protected void onHelloButtonClick() {
-    //welcomeText.setText("Welcome to JavaFX Application!");
-    //}
+    @FXML
+    private Button generateKeyEncryptFileButton;
+
+    @FXML
+    private Button generateKeyDecryptFileButton;
 }
--- View/src/main/java/module-info.java
+++ View/src/main/java/module-info.java
@@ -7,6 +7,8 @@ module com.example.view {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.datatransfer;
+    requires java.desktop;
 
     opens com.example.view to javafx.fxml;
     exports com.example.view;
